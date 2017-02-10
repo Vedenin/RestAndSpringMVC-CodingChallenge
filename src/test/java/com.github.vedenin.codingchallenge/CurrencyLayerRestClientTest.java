3c1f0e4cbf99ce96beac86b2c372ac0d6a803fda
vvedenin <vvedenin@luxoft.com>
2017-02-10 15:50:54 +0100

    Add ExternalErrorTest in RestTest

--- src/test/java/com.github.vedenin.codingchallenge/CurrencyLayerRestClientTest.java
+++ src/test/java/com.github.vedenin.codingchallenge/CurrencyLayerRestClientTest.java
@@ -1,6 +1,7 @@
 package com.github.vedenin.codingchallenge;
 
 import com.github.vedenin.codingchallenge.common.CurrencyEnum;
+import com.github.vedenin.codingchallenge.exceptions.RestClientException;
 import com.github.vedenin.codingchallenge.restclient.RestClient;
 import com.github.vedenin.codingchallenge.restclient.impl.currencylayer.CurrencyLayerRestClient;
 import org.junit.Assert;
@@ -23,6 +24,18 @@ public class CurrencyLayerRestClientTest {
         Assert.assertEquals(convertRates.doubleValue(), 42.6, 0.5);
         convertRates = restClient.getHistoricalExchangeRates(CurrencyEnum.EUR, CurrencyEnum.RUB, new GregorianCalendar(2012, 1, 1));
         Assert.assertEquals(convertRates.doubleValue(), 39.7, 0.5);
+    }
+
+    @Test
+    public void ExternalErrorTest() {
+        RestClient restClient = new CurrencyLayerRestClient();
+        try {
+            BigDecimal convertRates = restClient.getHistoricalExchangeRates(CurrencyEnum.EUR, CurrencyEnum.RUB, new GregorianCalendar(2110, 12, 47));
+            Assert.fail("Must be run RestClientException");
+        } catch (RestClientException exception) {
+            Assert.assertEquals("Error while gathering information from CurrencyLayer services, code:106, error:" +
+                    "Your query did not return any results. Please try again.", exception.getMessage());
+        }
     }
 
 }
